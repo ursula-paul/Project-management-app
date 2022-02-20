cfd1571acbf5c23deb9a64f9bbaea0adee6d0de7
ursula-paul <ursula.paul@decagon.dev>
2022-02-20 20:52:25 +0100

    added the delete and done functionalitiess

--- src/componets/SingleTodo.tsx
+++ src/componets/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Todo } from "./model";
 import { FiEdit } from "react-icons/fi";
 import { AiOutlineDelete } from "react-icons/ai";
@@ -12,18 +12,55 @@ type Props = {
 };
 
 export const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+	const [edit, setEdit] = useState<boolean>(false);
+	const [editTodo, setEditTodo] = useState<string>(todo.todo);
+	const handleDone = (id: number) => {
+		setTodos(
+			todos.map((todo) =>
+				todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+			)
+		);
+	};
+
+	const handleDelete = (id: number) => {
+		setTodos(todos.filter((todo) => todo.id !== id));
+	};
+
+	const handleEdit = (e: React.FormEvent, id: number) => {
+		e.preventDefault();
+
+		setTodos(todos.map((todo) => todo.id));
+	};
 	return (
-		<form className="todos__single">
-			<span className="todos__single--text">{todo.todo}</span>
+		<form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+			{edit ? (
+				<input
+					value={editTodo}
+					onChange={(e) => setEditTodo(e.target.value)}
+					className="todos__single--test"
+				/>
+			) : todo.isDone ? (
+				<s className="todos__single--text">{todo.todo}</s>
+			) : (
+				<span className="todos__single--text">{todo.todo}</span>
+			)}
 
 			<div>
-				<span className="icon">
+				<span
+					className="icon"
+					onClick={() => {
+						if (!edit && !todo.isDone) {
+							setEdit(!edit);
+						}
+					}}
+				>
 					<FiEdit />
 				</span>
 				<span className="icon">
+					<span className="icon" onClick={() => handleDelete(todo.id)}></span>
 					<AiOutlineDelete />
 				</span>
-				<span className="icon">
+				<span className="icon" onClick={() => handleDone(todo.id)}>
 					<IoCheckmarkDone />
 				</span>
 			</div>
